6c0fce109fc8dcd6ec4fcf8df41b73926117f129
Christian Vitali <vitali16@my.yorku.ca>
2019-01-15 00:08:19 -0500

    lab0part2

---
+++ est/src/est/Lab0Part2.java
@@ -0,0 +1,5 @@
+package est;
+
+public class Lab0Part2 {
+
+}
---
+++ est/src/est/helllo.java
@@ -0,0 +1,11 @@
+package est;
+
+public class helllo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
+
+}
