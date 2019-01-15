809d24a7a4a127ae2d3132e0c0ca046b88f3fefe
Christian Vitali <vitali16@my.yorku.ca>
2019-01-15 01:18:57 -0500

    lab0part2

---
+++ Lab0Part2/src/BooleanDataType.java
@@ -0,0 +1,22 @@
+
+public class BooleanDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// The bolean data type consists of
+		// only two values: true and false.
+		// true and false are the only two 
+		// possible boolean literals.
+		boolean p = true;
+		boolean q = false;
+		System.out.println("p is " + p);
+		System.out.println("q is " + q);
+		System.out.println("After re-assigning p to false, and q to true");
+		p = false;
+		q = false;
+		System.out.println("p is " + p);
+		System.out.println("q is " + q);
+
+	}
+
+}
---
+++ Lab0Part2/src/RelationalOperations.java
@@ -0,0 +1,13 @@
+
+public class RelationalOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean p = 3 < 5; // true because it is the case
+		boolean q = 5 < 3; // false because it is NOT the case
+		System.out.println("3 < 5 is " + p);
+		System.out.println("5 < 3 is " + q);
+
+	}
+
+}
